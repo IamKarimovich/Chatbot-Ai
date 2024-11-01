937780eeb50303e6f0990cf58935ee04de6182b9
Ruslan Karimov <98585383+IamKarimovich@users.noreply.github.com>
2024-11-01 12:27:51 +0400

    Let's Start

---
+++ src/main/java/com/ruslandev/aichatbox/ChatbotController.java
@@ -0,0 +1,4 @@
+package com.ruslandev.aichatbox;
+
+public class ChatbotController {
+}
---
+++ src/test/java/com/ruslandev/aichatbox/AiChatBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruslandev.aichatbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
